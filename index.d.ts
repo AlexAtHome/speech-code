a572016ef834fcd20cb0a0e0e2e383314fac9233
Alexander B <r3disrussian@gmail.com>
2019-01-14 00:04:04 +0800

    Added a declaration file

---
+++ index.d.ts
@@ -0,0 +1,9 @@
+// Type definitions for speech-code 1.0
+// Project: https://github.com/mamoru-kun/speech-code
+// Definitions by: Alexander B. <https://github.com/mamoru-kun>
+// Definitions: https://github.com/mamoru-kun/speech-code
+
+export function getText(paragraphs: Number): string
+export function getParagraph(sentences: Number): string
+
+export as namespace SpeechCode
