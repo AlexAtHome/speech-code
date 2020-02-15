e466323484b278619faa4e811e7627e655f3b1bb
Alexander Bolotskov <r3disrussian@gmail.com>
2020-02-15 19:07:12 +0800

    fix: 🐛 add comments to definition file

--- index.d.ts
+++ index.d.ts
@@ -3,7 +3,16 @@
 // Definitions by: Alexander B. <https://github.com/mamoru-kun>
 // Definitions: https://github.com/mamoru-kun/speech-code
 
-export function getText(paragraphs: Number): string
-export function getParagraph(sentences: Number): string
-
-export as namespace SpeechCode
+export namespace SpeechCode {
+  /**
+   * Returns a text consisting of given number of paragraphs
+   * consisting of random number of sentences (from 2 to 5)
+   * @param paragraphs - the number of paragraphs in the text
+   */
+  export function getText(paragraphs?: Number = 3): string;
+  /**
+   * Returns a paragraph consisting of given number of sentences
+   * @param sentences - the number of sentences
+   */
+  export function getParagraph(sentences: Number): string;
+}
--- index.js
+++ index.js
@@ -1,57 +1,55 @@
-'use strict'
+"use strict";
 
-const table = require('./sentence-parts.json')
+const sentenceParts = require("./sentence-parts.json");
 
-/** Получает рандомной значение из массива */
-const random = array => {
-  const i = ~~(array.length * Math.random())
-  return array[i]
-}
+const randomValueFrom = array => {
+  let index = ~~(array.length * Math.random());
+  return array[index];
+};
 
 /**
  * Выстраивает предложение
  */
 const buildSentence = () => {
-  let result = [
-    random(table.first),
-    random(table.second),
-    random(table.third),
-    random(table.fourth)
-  ]
-  if (result[0].endsWith('!') || result[0].endsWith('.')) {
-    result[1] = result[1].replace(/^./, result[1].slice(0, 1).toUpperCase())
+  let result = [];
+  result.push(randomValueFrom(sentenceParts.first));
+  result.push(randomValueFrom(sentenceParts.second));
+  result.push(randomValueFrom(sentenceParts.third));
+  result.push(randomValueFrom(sentenceParts.fourth));
+  if (result[0].endsWith("!") || result[0].endsWith(".")) {
+    result[1] = result[1].replace(/^./, result[1].slice(0, 1).toUpperCase());
   }
-  return result.join(' ')
-}
+  return result.join(" ");
+};
 
 /**
  * Строит абзац из предложений
  * @param {number} sentences Число предложений в абзаце
  */
 const getParagraph = sentences => {
-  let result = ''
+  let result = "";
   for (let i = 0; i < sentences; i++) {
-    if (i > 0) result += ' '
-    result += buildSentence()
+    if (i > 0) result += " ";
+    result += buildSentence();
   }
-  return result
-}
+  return result;
+};
 
 /**
  * Строит текст
  * @param {number} paragraphs Число абзацев в тексте
  */
 const getText = (paragraphs = 3) => {
-  let result = ''
+  let result = "";
   for (let i = 0; i < paragraphs; i++) {
-    if (i > 0) result += '\n'
-    let sentences = Math.floor(Math.random() * 5) + 2
-    result += getParagraph(sentences)
+    if (i > 0) result += "\n";
+    let sentences = Math.floor(Math.random() * 5) + 2;
+    result += getParagraph(sentences);
   }
-  return result
-}
+  return result;
+};
 
 module.exports = {
   getParagraph,
   getText
-}
+};
