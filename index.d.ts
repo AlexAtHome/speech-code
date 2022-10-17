cf03ee3a322929bd619e956b1a4cbc04fe36dcb6
Alexander Bolotskov <mamoruuu@proton.me>
2022-10-17 23:24:14 +0600

    fix: 🐛 fixes typings

--- index.d.ts
+++ index.d.ts
@@ -10,22 +10,25 @@ export type Lang = 'ru' | 'en';
 /**
  * Returns a text consisting of given number of paragraphs
  * consisting of random number of sentences (from 2 to 5)
- * @param paragraphs - the number of paragraphs in the text. Default is 3.
+ * @param paragraphs The number of paragraphs in the text. Default is 3.
+ * @param language The language to generate a text in
  */
-export function getText(paragraphs?: number, language: Lang = 'ru'): string;
+export function getText(paragraphs?: number, language?: Lang): string;
 /**
  * Returns a paragraph consisting of given number of sentences
- * @param sentences - the number of sentences. Default is 3.
+ * @param sentences The number of sentences. Default is 3.
+ * @param language The language to generate a text in
  */
-export function getParagraph(sentences?: number, language: Lang = 'ru'): string;
+export function getParagraph(sentences?: number, language?: Lang): string;
 /**
  * Returns a single generated sentence
+ * @param language The language to generate a text in
  */
-export function getSentence(language: Lang = 'ru'): string;
+export function getSentence(language?: Lang): string;
 /**
  * Returns a paragraph with specified number of characters.
  *
  * *WARNING: The length of the resulting text will never be equal to specified value. It might be either shorter or longer.*
- * @param chars - the number of characters (spaces included). Default is 50.
+ * @param chars The number of characters (spaces included). Default is 50.
  */
-export function getCharacters(chars?: number, language: Lang = 'ru'): string;
+export function getCharacters(chars?: number, language?: Lang): string;
